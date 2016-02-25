fc463e99f5b05f10787444d6c5e666b470248a3e
梁鹏飞 <871999566@qq.com>
2016-02-26 00:07:34 +0800

    add view

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ChartAndCircleBar
 This is a chart and circleBar view to show win draw and lose between two teams.
+![view](https://raw.githubusercontent.com/liangpengfei/ChartAndCircleBar/master/screenshots/ll.gif)
--- app/src/main/java/xhome/uestcfei/com/chart/MainActivity.java
+++ app/src/main/java/xhome/uestcfei/com/chart/MainActivity.java
@@ -2,13 +2,7 @@ package xhome.uestcfei.com.chart;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 /**
